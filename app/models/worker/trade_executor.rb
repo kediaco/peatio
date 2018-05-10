92db8c752e73b1a0702fb8ba78b725d3fa57f5bb
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-10 16:55:07 +0300

    Fix trade execution errors (#1071)
    * Fix trade execution errors

--- app/models/worker/trade_executor.rb
+++ app/models/worker/trade_executor.rb
@@ -1,13 +1,11 @@
 module Worker
   class TradeExecutor
-
     def process(payload, metadata, delivery_info)
       payload.symbolize_keys!
-      ::Matching::Executor.new(payload).execute!
+      ::Matching::Executor.new(payload).execute
     rescue
       SystemMailer.trade_execute_error(payload, $!.message, $!.backtrace.join("\n")).deliver
       raise $!
     end
-
   end
 end
