50fa372da65a5d8775b291db6d903116bfa4f435
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-21 20:12:21 +0300

    Send CORS headers from API v2 ever on error (closes #1113). (#1149)

--- app/api/api_v2/cors/middleware.rb
+++ app/api/api_v2/cors/middleware.rb
@@ -1,19 +1,37 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
 module APIv2
   module CORS
+    class << self
+      def call(headers)
+        headers.reverse_merge!(self.headers)
+
+        # Response may differ if server specifies "*" as allowed origins.
+        # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+        if headers['Access-Control-Allow-Origin'] != '*'
+          headers['Vary'] = [headers['Vary'], 'Origin'].compact.join(', ')
+        end
+
+        headers
+      end
+
+      def headers
+        { 'Access-Control-Allow-Origin'      => ENV.fetch('API_CORS_ORIGINS'),
+          'Access-Control-Allow-Methods'     => 'GET, POST, PUT, PATCH, DELETE',
+          'Access-Control-Allow-Headers'     => 'Origin, X-Requested-With, Content-Type, Accept, Authorization' }
+      end
+    end
+
     class Middleware < Grape::Middleware::Base
       def call(env)
         request = Grape::Request.new(env)
         if request.options?
-          [200, cors, []]
+          [200, APIv2::CORS.headers, []]
         else
           response = @app.call(env)
-          response.headers.reverse_merge!(cors)
-
-          # Response may differ if server specifies "*" as allowed origins.
-          # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
-          if response.headers['Access-Control-Allow-Origin'] != '*'
-            response.headers['Vary'] = [response.headers['Vary'], 'Origin'].compact.join(', ')
-          end
+          headers  = Array === response ? response[1] : response.headers
+          APIv2::CORS.call(headers)
           response
         end
       end
@@ -22,17 +40,6 @@ module APIv2
 
       def request
         @request ||= Grape::Request.new(env)
-      end
-
-      def cors
-        { 'Access-Control-Allow-Origin'  => cors_origins,
-          'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE',
-          'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        }
-      end
-
-      def cors_origins
-        ENV.fetch('API_CORS_ORIGINS')
       end
     end
   end
