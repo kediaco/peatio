20868ab10d27b55fec7ba5824dc95addc578cd88
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-06-03 19:59:51 +0300

    Add to Currency model description, homepage and price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_130534) do
+ActiveRecord::Schema.define(version: 2020_06_03_164002) do
 
   create_table "accounts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "member_id", null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2020_05_27_130534) do
 
   create_table "currencies", id: :string, limit: 10, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.text "description"
+    t.string "homepage"
     t.string "blockchain_key", limit: 32
     t.string "symbol", limit: 1, null: false
     t.string "type", limit: 30, default: "coin", null: false
@@ -104,6 +106,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_130534) do
     t.bigint "base_factor", default: 1, null: false
     t.integer "precision", limit: 1, default: 8, null: false
     t.string "icon_url"
+    t.decimal "price", precision: 32, scale: 16
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["position"], name: "index_currencies_on_position"
