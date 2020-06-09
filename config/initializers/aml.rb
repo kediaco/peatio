499e5a837bdc7027f2c1dedc66df2f63ad35ab7d
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-06-09 15:48:46 +0300

    Add bitcoin specs with from_addresses

--- config/initializers/aml.rb
+++ config/initializers/aml.rb
@@ -3,7 +3,8 @@ require 'peatio/aml'
 
 begin
   if ENV['AML_BACKEND'].present?
-    Peatio::AML.adapter = "Peatio::AML::#{ENV.fetch('AML_BACKEND').capitalize}".constantize.new
+    require ENV['AML_BACKEND']
+    Peatio::AML.adapter = "#{ENV.fetch('AML_BACKEND').capitalize}".constantize.new
   end
 rescue StandardError => e
   Rails.logger.error { e.message }
